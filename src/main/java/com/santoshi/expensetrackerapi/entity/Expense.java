d0068461a147075770465330e8afc97d1b0fa429
santoshi-1 <karuturisantoshi@gmail.com>
2023-08-13 16:49:05 -0400

    created api's to perform crud operations

--- src/main/java/com/santoshi/expensetrackerapi/entity/Expense.java
+++ src/main/java/com/santoshi/expensetrackerapi/entity/Expense.java
@@ -4,9 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 @Data // getter and setters
 @NoArgsConstructor
@@ -17,7 +21,7 @@ public class Expense {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "expense_name")
     private String name;
@@ -30,5 +34,13 @@ public class Expense {
     private String category;
 
     private Date date;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
 }
