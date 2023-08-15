bd9beaf66aa21b2c9f4366f83f735e181480f7c8
Santoshi Karuturi <66348280+santoshi-1@users.noreply.github.com>
2023-08-14 20:45:44 -0400

    added JPA query methods without the need to write SQL queries (#5)

--- src/main/java/com/santoshi/expensetrackerapi/repository/ExpenseRepository.java
+++ src/main/java/com/santoshi/expensetrackerapi/repository/ExpenseRepository.java
@@ -1,9 +1,23 @@
 package com.santoshi.expensetrackerapi.repository;
 
 import com.santoshi.expensetrackerapi.entity.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    //Select * from tbl_expenses where category=category_name
+    Page<Expense> findByCategory(String category, Pageable pageable);
+
+    //select * from tbl_expenses where name LIKE '%keyword%'
+    Page<Expense> findByNameContaining(String keyword, Pageable pageable);
+
+    //Select * form tbl_expenses where date BETWEEN 'startDate' and 'endDate'
+    Page<Expense> findByDateBetween(Date startDate, Date endDate, Pageable pageable);
+
 }
--- src/main/java/com/santoshi/expensetrackerapi/service/ExpenseService.java
+++ src/main/java/com/santoshi/expensetrackerapi/service/ExpenseService.java
@@ -4,6 +4,7 @@ import com.santoshi.expensetrackerapi.entity.Expense;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface ExpenseService {
@@ -17,5 +18,11 @@ public interface ExpenseService {
     Expense saveExpenseDetails(Expense expense);
 
     Expense updateExpenseDetails(Long id, Expense expense);
+
+    List<Expense> readByCategory(String category, Pageable pageable);
+
+    List<Expense> searchExpenseByKeyword(String keyword, Pageable pageable);
+
+    List<Expense> readByDate(Date startDate, Date endDate, Pageable pageable);
 
 }
