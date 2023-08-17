1a6d345582f50be0cb92f47d87075388a4a69bd1
santoshi-1 <karuturisantoshi@gmail.com>
2023-08-17 08:20:08 -0400

    added few modifications to  user class and by crypted the password before storing into database

--- src/main/java/com/santoshi/expensetrackerapi/controller/UserController.java
+++ src/main/java/com/santoshi/expensetrackerapi/controller/UserController.java
@@ -16,11 +16,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@Valid @RequestBody UserModal userModal) {
-        return new ResponseEntity<User>(userService.registerUser(userModal), HttpStatus.CREATED);
-    }
-
     @GetMapping("/users/{id}")
     public ResponseEntity<User> get(@PathVariable Long id) {
         return new ResponseEntity<User>(userService.readUser(id), HttpStatus.OK);
