0387f6df811eb384281476eb9f3e22be8d42e402
Santoshi Karuturi <66348280+santoshi-1@users.noreply.github.com>
2023-08-18 09:04:46 -0400

    Jwt authentication (#8)
    * Creating JWT token
    
    * Validating JWT token for all requests

--- src/main/java/com/santoshi/expensetrackerapi/config/SecurityConfiguration.java
+++ src/main/java/com/santoshi/expensetrackerapi/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.santoshi.expensetrackerapi.config;
 
 import com.santoshi.expensetrackerapi.security.CustomUserDetailsService;
+import com.santoshi.expensetrackerapi.security.JWTRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -20,6 +23,13 @@ public class SecurityConfiguration {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    ;
+
+    @Bean
+    public JWTRequestFilter authenticationJwtTokenFilter() {
+        return new JWTRequestFilter();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -29,7 +39,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable());
-        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("/login", "/register").permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults()        );
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("/login", "/register").permitAll().anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))   ;
+        httpSecurity.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
--- src/main/java/com/santoshi/expensetrackerapi/controller/AuthController.java
+++ src/main/java/com/santoshi/expensetrackerapi/controller/AuthController.java
@@ -1,17 +1,23 @@
 package com.santoshi.expensetrackerapi.controller;
 
 import com.santoshi.expensetrackerapi.entity.AuthModal;
+import com.santoshi.expensetrackerapi.entity.JWTResponse;
 import com.santoshi.expensetrackerapi.entity.User;
 import com.santoshi.expensetrackerapi.entity.UserModal;
+import com.santoshi.expensetrackerapi.security.CustomUserDetailsService;
 import com.santoshi.expensetrackerapi.service.UserService;
+import com.santoshi.expensetrackerapi.util.JwtTokenUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +29,30 @@ public class AuthController {
     private UserService userService;
 
     @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody AuthModal authModal) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModal.getEmail(), authModal.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    public ResponseEntity<JWTResponse> login(@RequestBody AuthModal authModal) throws Exception {
+        authenticate(authModal.getEmail(), authModal.getPassword());
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(authModal.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return new ResponseEntity<JWTResponse>(new JWTResponse(token), HttpStatus.OK);
+    }
+
+    public void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (DisabledException exception) {
+            throw new Exception("User Disabled!");
+        } catch (BadCredentialsException exception) {
+            throw new Exception("Bad Credentials");
+        }
     }
 
     @PostMapping("/register")
